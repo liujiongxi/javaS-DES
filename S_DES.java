ee08075e6857afc8760b7675410d57a92c0f4692
liujiongxi <135441287+liujiongxi@users.noreply.github.com>
2024-10-07 16:12:27 +0800

    Add files via upload

---
+++ S_DES.java
@@ -0,0 +1,9 @@
+
+public class S_DES {
+    public static void main(String[] args) {
+        Window win = new Window();
+        win.setTitle("S-DES");
+        win.setBounds(200,200,700,400);
+
+    }
+}
