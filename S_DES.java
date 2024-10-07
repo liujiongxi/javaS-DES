f3132fc73cd4be69164a661e7fdc1bf584abd09b
liujiongxi <135441287+liujiongxi@users.noreply.github.com>
2024-10-07 17:11:07 +0800

    Delete S_DES.java

--- S_DES.java
+++
@@ -1,9 +0,0 @@
-
-public class S_DES {
-    public static void main(String[] args) {
-        Window win = new Window();
-        win.setTitle("S-DES");
-        win.setBounds(200,200,700,400);
-
-    }
-}
